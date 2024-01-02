dae3003d8ed093b4a1c6be16da79b6fa175401f4
Kashif Ullah <113616575+Kashif-Ullah1@users.noreply.github.com>
2024-01-02 16:19:54 +0500

    grab the code

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import MealsSummary from './MealsSummary';
+import AvailibleMeals from './AvailibleMeals';
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailibleMeals/>
+    </Fragment>
+  )
+}
+
+export default Meals
